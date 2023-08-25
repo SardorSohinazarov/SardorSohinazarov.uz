a1c64850532a851742594b06a3288df0bcbf4b67
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-08-25 06:21:50 +0500

    Tamom uje

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var darkModeToggle = document.getElementById('darkModeToggle');
+var body = document.body;
+
+darkModeToggle.addEventListener('click', function () {
+    body.classList.toggle('dark-mode');
+});
